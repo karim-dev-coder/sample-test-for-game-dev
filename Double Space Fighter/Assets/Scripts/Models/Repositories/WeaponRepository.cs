4ed3d7245d7b7116784098499c050658e70a760e
Karim <karim.kimsanbaev@gmail.com>
2023-02-25 21:53:21 +0300

    feat(weapon): add weapons

--- Double Space Fighter/Assets/Scripts/Models/Repositories/WeaponRepository.cs
+++ Double Space Fighter/Assets/Scripts/Models/Repositories/WeaponRepository.cs
@@ -5,7 +5,10 @@ public static class WeaponRepository
     private static readonly Dictionary<string, IWeapon> _repository = new()
     {
         [EmptyWeapon.ID] = new EmptyWeapon(),
-        ["weapon_5_dmg_3_cd"] = new SimpleWeapon("weapon_5_dmg_3_cd", 5, 3)
+        ["weapon_5_dmg_3_cd"] = new SimpleWeapon("weapon_5_dmg_3_cd", 5, 3),
+        ["weapon_4_dmg_2_cd"] = new SimpleWeapon("weapon_4_dmg_2_cd", 4, 2),
+        ["weapon_20_dmg_5_cd"] = new SimpleWeapon("weapon_20_dmg_5_cd", 5, 20),
+        ["weapon_20_dmg_0.5_cd"] = new SimpleWeapon("weapon_20_dmg_0.5_cd", 20, 0.5f)
     };
 
     public static IWeapon Create(string id)
